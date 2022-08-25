750658416af2e35746fb0f64918eb6fbc210effa
akinaykut05 <akinaykut05@gmail.com>
2022-08-25 09:53:50 +0300

    Added project files

---
+++ my-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    TheHeader,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ my-project/src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    <h1>RememberMe</h1>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  height: 15%;
+  background-color: rgb(171, 9, 44);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
