d9b8b8d46d4ba441e47e2a4f7b8ba4831226f7d7
akinaykut05 <akinaykut05@gmail.com>
2022-08-28 14:47:54 +0300

    Updated a few files

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -2,27 +2,22 @@
   <div>
     <the-header></the-header>
 
-    <router-view></router-view>
-
-    <hello-world></hello-world>
-    <zilan-section></zilan-section>
+    <main>
+      <router-view></router-view>
+    </main>
     <the-footer></the-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import TheFooter from "./components/TheFooter.vue";
 import TheHeader from "./components/TheHeader.vue";
-import ZilanSection from "./components/ZilanSection.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     TheHeader,
     TheFooter,
-    ZilanSection,
   },
 };
 </script>
@@ -33,12 +28,9 @@ body {
   padding: 0;
   box-sizing: border-box;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: bisque;
-  height: 100vh;
+
+main {
+  display: flex;
+  width: 100%;
 }
 </style>
--- my-project/src/components/TheHome.vue
+++ my-project/src/components/TheHome.vue
@@ -1,3 +1,54 @@
 <template>
-  <h1>Home</h1>
+  <div class="asd">
+    <section v-for="item in data" :key="item.id">
+      <the-card>
+        <!-- <img :src="item.thumbnail" alt="" />
+        <a :href="item.thumbnail" target="_blank">
+          <h4>{{ item.title }}</h4>
+        </a>
+        <p>{{ item.description }}</p>
+        <span>{{ item.price }}</span> -->
+      </the-card>
+    </section>
+  </div>
 </template>
+
+<script>
+import TheCard from "./TheCard.vue";
+
+export default {
+  components: {
+    TheCard,
+  },
+  data() {
+    return {
+      data: [],
+    };
+  },
+  beforeCreate() {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        this.data = json.products;
+        console.log(this.data);
+        for (let i in this.data) {
+          console.log(this.data[i].thumbnail);
+        }
+      });
+  },
+};
+</script>
+
+<style scoped>
+.asd {
+  width: 100%;
+}
+.asd section {
+  display: inline-block;
+  width: 24%;
+  height: 24%;
+  background-color: #ab092c;
+  padding: 2rem;
+  margin: 1%;
+}
+</style>
